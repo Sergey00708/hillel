5fa15f99886ac17e82259416a79d3378b0e0e7fc
Sergey <Fenix00708@gmail.com>
2022-04-06 18:08:59 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+class people
+{
+   public $name;
+   public $weight;
+   public $age;
+
+   public function __construct($name, $age, $weight)
+   {
+       $this->name = $name;
+       $this->age = $age;
+       $this->weight = $weight;
+   }
+
+   public function about ()
+   {
+       echo 'name:'. ' '.$this->name. "<br>";
+       echo 'age:'.' '.$this->age. "<br>";
+       echo 'weight:'.' '.$this->weight;
+   }
+}
+
+$sergey = new people('sergey',31, 72);
+$sergey->about();
+
